348cc8fab860354924d68fd3f38197b07fed3042
jiheon2 <000224kjh@gmail.com>
2025-03-24 09:48:45 +0900

    Jenkins Test Code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,10 +2,31 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins!");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello, Jenkins!");
+            System.out.println("5");
+            System.out.println("4");
+            System.out.println("3");
+            System.out.println("2");
+            System.out.println("1");
+            if (i == 9) {
+                System.out.println("프로그램을 종료합니다.");
+            }
+        }
     }
 
     public String getGreeting() {
-        return "Hello, Jenkins!";
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello, Jenkins!");
+            System.out.println("5");
+            System.out.println("4");
+            System.out.println("3");
+            System.out.println("2");
+            System.out.println("1");
+            if (i == 9) {
+                System.out.println("프로그램을 종료합니다.");
+            }
+        }
+        return "finish";
     }
 }
